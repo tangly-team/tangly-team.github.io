a99e6aed3bd317f8cf46b52939a8c4cb7d1efcf9
Marcel Baumann <marcelbaumann@gmail.com>
2024-11-02 14:04:16 +0100

    new release of the tangly-team web site

--- docs/domains/ui/api-ui/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"net.tangly.erp.ui","l":"AppShell"},{"p":"net.tangly.erp.ui","l":"Main"},{"p":"net.tangly.erp.ui","l":"MainView"}];updateSearchResults();
